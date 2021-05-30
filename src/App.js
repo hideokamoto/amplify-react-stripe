722cc1a6ba5b69b15c32f7de2c5dfca1005ee9cd
Hidetaka Okamoto <info@wp-kyoto.net>
2021-05-30 23:35:34 +0900

    init bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'reactstrap';
 import { Products } from './Products';
 
 function App() {
   return (
     <div className="App">
-      <Products />
+      <Container>
+        <Products />
+      </Container>
     </div>
   );
 }
