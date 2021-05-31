a395e2e43f2e30872b0da4617e63d3cc2196cda0
Hidetaka Okamoto <info@wp-kyoto.net>
2021-05-31 19:18:56 +0900

    create customer

--- src/BuyButton.jsx
+++ src/BuyButton.jsx
@@ -1,24 +1,25 @@
-import {API} from 'aws-amplify'
+import { API, Auth } from 'aws-amplify'
 import { Button, Form } from 'reactstrap';
 import {loadStripe} from '@stripe/stripe-js';
 
 
 const redirectToStripeCheckout = async (priceId, type) => {
-    const checkoutSession = await API.post('stripeapi', `/products/${priceId}/checkout`, {
+    const checkout = await API.post('stripeapi', `/products/${priceId}/checkout`, {
         body: {
             type
         }
     })
     const stripe = await loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY)
-    const result = await stripe.redirectToCheckout({ sessionId: checkoutSession.id })
+    const result = await stripe.redirectToCheckout({ sessionId: checkout.session.id })
     if (result.error) throw new Error(result.error.message)
+
+
     return;
 }
 
 export function BuyButton({price}) {
     const handleSubmit = (event) => {
         event.preventDefault()
-        console.log(price.type)
         redirectToStripeCheckout(price.id, price.type)
             .catch(e => {
                 console.log(e)
